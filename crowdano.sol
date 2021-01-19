11d01c7963fd3c1ddf1ebfcd6e1099123fc4f8d5
ejane24 <56359025+ejane24@users.noreply.github.com>
2021-01-19 23:23:34 +0000

    add withdrawal function to campaign contract

--- crowdano.sol
+++ crowdano.sol
@@ -9,13 +9,17 @@ contract Campaign {
     uint public goal;
     uint public deadline
     address payable public owner;
-    mapping(address => uint) public pledgeAmounts
+    mapping(address => uint) public pledgeAmounts;
+    uint public pendingWithdrawl;
+    bool public failed;
 
     constructor(uint _id, uint _goal, uint _deadline, address payable _owner) public {
         id = _id;
         goal = _goal;
         deadline = _deadline;
         owner = _owner;
+        failed = false;
+        pendingWithdrawal = 0;
     }
 
     function support() public payable {
@@ -23,5 +27,22 @@ contract Campaign {
         pledgeAmounts[msg.sender] = msg.value;
     }
 
+    function checkFailed() public {
+        require(block.timestamp >= deadline);
+        if(this.balance < goal) {
+            failed = true;
+        }
+    }
 
+    function withdraw() public {
+        if(failed) {
+            uint amount = pledgeAmounts[msg.sender],
+            pledgeAmounts[msg.sender] = 0;
+            msg.sender.transfer(amount);
+        } else if(msg.sender == owner) {
+            uint amount = pendingWithdrawal;
+            pendingWithdrawal = 0;
+            msg.sender.trabsfer(amount);
+        }
+    }
 }
