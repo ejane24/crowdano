5de417387b9e30824bfe0beed3cb65e418535ac0
ejane24 <56359025+ejane24@users.noreply.github.com>
2021-01-19 22:51:37 +0000

    Add constructor and support functions

--- crowdano.sol
+++ crowdano.sol
@@ -1,2 +1,27 @@
 pragma solidity ^0.7.0;
 
+contract Crowdano {
+
+}
+
+contract Campaign {
+    uint public id;
+    uint public goal;
+    uint public deadline
+    address payable public owner;
+    mapping(address => uint) public pledgeAmounts
+
+    constructor(uint _id, uint _goal, uint _deadline, address payable _owner) public {
+        id = _id;
+        goal = _goal;
+        deadline = _deadline;
+        owner = _owner;
+    }
+
+    function support() public payable {
+        require(block.timestamp < deadline);
+        pledgeAmounts[msg.sender] = msg.value;
+    }
+
+
+}
